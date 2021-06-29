2cb4c3281e0726df7c34654892c5fcccbe10feb2
Nicholas Lee <niconal902@gmail.com>
2021-06-29 10:00:17 -0700

    feat: initial init of repo and push of react app skeleton

---
+++ neurosynth/src/App.tsx
@@ -0,0 +1,28 @@
+// import React from 'react';
+import logo from './assets/images/logo.png';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="left">
+
+        <div className="login-form-container">
+          <div className="login-form">
+            <div className="">
+              {/* placeholder for login form */}
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div className="right">
+        <div className="logo-container">
+          <h1>welcome to neurosynth</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
