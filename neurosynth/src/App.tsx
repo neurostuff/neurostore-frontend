51676777f33e89da6d6a7746bee4942ea02c76a0
Nicholas Lee <niconal902@gmail.com>
2021-07-07 09:02:14 -0700

    fix: deleted project and restarted with latest create-react-scripts version

--- neurosynth/src/App.tsx
+++ neurosynth/src/App.tsx
@@ -1,26 +1,24 @@
-// import React from 'react';
-import logo from './assets/images/logo.png';
-import './App.scss';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <div className="left">
-
-        <div className="login-form-container">
-          <div className="login-form">
-            <div className="">
-              {/* placeholder for login form */}
-            </div>
-          </div>
-        </div>
-
-      </div>
-      <div className="right">
-        <div className="logo-container">
-          <h1>welcome to neurosynth</h1>
-        </div>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
