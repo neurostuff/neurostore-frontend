2c58017ef19a88ba375bc3ea02bcf58363eb526f
Nicholas Lee <niconal902@gmail.com>
2021-07-13 12:47:27 -0400

    feat: added basic search bar

--- neurosynth/src/App.tsx
+++ neurosynth/src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import classes from "./App.module.css";
+import { IStudy } from "./models/study";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [studies, setStudies] = useState<IStudy[]>();
+
+    const getStudies = () => {
+        const url = "https://neurostore.org/api/studies";
+
+        axios.get<IStudy[]>(url).then((res) => {
+            setStudies(res.data);
+            console.log(res.data);
+        });
+    };
+
+    useEffect(() => {
+        getStudies();
+    }, []);
+
+    return (
+        <>
+            <div className={classes["search-bar-container"]}>
+                <TextField
+                    placeholder="Search for your study"
+                    variant="outlined"
+                    id="outlined-basic"
+                    className={classes["search-bar"]}
+                ></TextField>
+            </div>
+            <p>
+                {studies?.map((x) => (
+                    <div>
+                        <span>{x.name}</span>
+                        <br />
+                    </div>
+                ))}
+            </p>
+        </>
+    );
 }
 
 export default App;
